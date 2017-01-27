7f450ba4f82ebb391d6661bf7bd1174829a7b18a
Raman Singh <Raman@MacBook-Pro.local>
2017-01-27 10:47:26 -0800

    hello world

---
+++ testing/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  testing
+//
+//  Created by Raman Singh on 2017-01-27.
+//  Copyright © 2017 Raman Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("A")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
